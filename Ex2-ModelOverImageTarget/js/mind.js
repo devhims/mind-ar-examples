faac494b77cd5e7d6ad09d92e10eb5f587f08320
Himanshu Gupta <thinktank.himanshu@gmail.com>
2021-03-16 14:54:35 +0530

    example 2 cleanup

--- Ex2-ModelOverImageTarget/js/mind.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const sceneEl = document.querySelector('a-scene');
-  const arSystem = sceneEl.systems['mindar-system'];
-
-  sceneEl.addEventListener('arReady', (event) => {
-    arSystem.start();
-  });
-});
